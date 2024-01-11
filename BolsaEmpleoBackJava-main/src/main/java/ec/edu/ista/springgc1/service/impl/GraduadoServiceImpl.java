7ee5c64e97b5cf32f39b24a914840b1a3622ca97
LeninCr <lenin.cordova.est@tecazuay.edu.ec>
2024-01-11 01:14:34 -0500

    Update the repository and ServiceImpl of Graduate
    We added new changes to the repository and search service for graduates by ID

--- BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/service/impl/GraduadoServiceImpl.java
+++ BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/service/impl/GraduadoServiceImpl.java
@@ -1,5 +1,6 @@
 package ec.edu.ista.springgc1.service.impl;
-import   ec.edu.ista.springgc1.model.entity.*;
+
+import ec.edu.ista.springgc1.model.entity.*;
 import ec.edu.ista.springgc1.repository.CiudadRepository;
 import ec.edu.ista.springgc1.repository.GraduadoRepository;
 import ec.edu.ista.springgc1.repository.UsuarioRepository;
@@ -14,93 +15,97 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ec.edu.ista.springgc1.exception.ResourceNotFoundException;
-import  ec.edu.ista.springgc1.model.dto.*;
+import ec.edu.ista.springgc1.model.dto.*;
+
 @Service
-public class GraduadoServiceImpl extends GenericServiceImpl<Graduado > implements Mapper<Graduado, GraduadoDTO>{
-	  @Autowired
+public class GraduadoServiceImpl extends GenericServiceImpl<Graduado> implements Mapper<Graduado, GraduadoDTO> {
+	@Autowired
 	private GraduadoRepository graduadoRepository;
-	  @Autowired
-	    private UsuarioRepository usuarioRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
-	    @Autowired
-	    private CiudadRepository ciudadRepository;
+	@Autowired
+	private CiudadRepository ciudadRepository;
 
-	    @Autowired
-	    private S3Service s3Service;
+	@Autowired
+	private S3Service s3Service;
 
-	    @Override
-	    public Graduado mapToEntity(GraduadoDTO estudianteDTO) {
-	    	Graduado estudiante = new Graduado();
+	@Override
+	public Graduado mapToEntity(GraduadoDTO estudianteDTO) {
+		Graduado estudiante = new Graduado();
 
-	        Usuario usuario = usuarioRepository.findBynombreUsuario(estudianteDTO.getUsuario())
-	                .orElseThrow(() -> new ResourceNotFoundException("usuario", estudianteDTO.getUsuario()));
+		Usuario usuario = usuarioRepository.findBynombreUsuario(estudianteDTO.getUsuario())
+				.orElseThrow(() -> new ResourceNotFoundException("usuario", estudianteDTO.getUsuario()));
 
-	        Ciudad ciudad = ciudadRepository.findByNombre(estudianteDTO.getCiudad())
-	                .orElseThrow(() -> new ResourceNotFoundException("ciudad", estudianteDTO.getCiudad()));
+		Ciudad ciudad = ciudadRepository.findByNombre(estudianteDTO.getCiudad())
+				.orElseThrow(() -> new ResourceNotFoundException("ciudad", estudianteDTO.getCiudad()));
 
-	        estudiante.setId(estudianteDTO.getId());
-	        estudiante.setUsuario(usuario);
-	       
-	        estudiante.setCiudad(ciudad);
-	        estudiante.setAño_graduacion(estudianteDTO.getAño_graduacion());
-	        estudiante.setEmail_personal(estudianteDTO.getEmail_personal());
-	        estudiante.setEstadocivil(estudianteDTO.getEstadocivil());
-	        estudiante.setRuta_pdf(estudianteDTO.getRuta_pdf());
-	        estudiante.setUrl_pdf(estudianteDTO.getRuta_pdf() == null ? null : s3Service.getObjectUrl(estudianteDTO.getRuta_pdf()));
+		estudiante.setId(estudianteDTO.getId());
+		estudiante.setUsuario(usuario);
 
-	        return estudiante;
-	    }
+		estudiante.setCiudad(ciudad);
+		estudiante.setAño_graduacion(estudianteDTO.getAño_graduacion());
+		estudiante.setEmail_personal(estudianteDTO.getEmail_personal());
+		estudiante.setEstadocivil(estudianteDTO.getEstadocivil());
+		estudiante.setRuta_pdf(estudianteDTO.getRuta_pdf());
+		estudiante.setUrl_pdf(
+				estudianteDTO.getRuta_pdf() == null ? null : s3Service.getObjectUrl(estudianteDTO.getRuta_pdf()));
 
-	    @Override
-	    public GraduadoDTO mapToDTO(Graduado estudiante) {
-	    	GraduadoDTO estudianteDTO = new GraduadoDTO();
+		return estudiante;
+	}
 
-	        estudianteDTO.setId(estudiante.getId());
-	        estudianteDTO.setUsuario(estudiante.getUsuario().getNombreUsuario());
-	        estudianteDTO.setCiudad(estudiante.getCiudad().getNombre());
-	        estudianteDTO.setAño_graduacion(estudiante.getAño_graduacion());
-	        estudianteDTO.setEmail_personal(estudiante.getEmail_personal());
-	        estudianteDTO.setEstadocivil(estudiante.getEstadocivil());
-	        estudianteDTO.setRuta_pdf(estudiante.getRuta_pdf());
-	        estudianteDTO.setUrl_pdf(estudiante.getUrl_pdf());
+	@Override
+	public GraduadoDTO mapToDTO(Graduado estudiante) {
+		GraduadoDTO estudianteDTO = new GraduadoDTO();
 
-	        return estudianteDTO;
-	    }
+		estudianteDTO.setId(estudiante.getId());
+		estudianteDTO.setUsuario(estudiante.getUsuario().getNombreUsuario());
+		estudianteDTO.setCiudad(estudiante.getCiudad().getNombre());
+		estudianteDTO.setAño_graduacion(estudiante.getAño_graduacion());
+		estudianteDTO.setEmail_personal(estudiante.getEmail_personal());
+		estudianteDTO.setEstadocivil(estudiante.getEstadocivil());
+		estudianteDTO.setRuta_pdf(estudiante.getRuta_pdf());
+		estudianteDTO.setUrl_pdf(estudiante.getUrl_pdf());
 
-	    @Override
-	    public List findAll() {
-	        return graduadoRepository.findAll()
-	                .stream()
-	                .peek(e -> e.setUrl_pdf(e.getRuta_pdf() == null ? null : s3Service.getObjectUrl(e.getRuta_pdf())))
-	                .map(e -> mapToDTO(e))
-	                .collect(Collectors.toList());
-	    }
+		return estudianteDTO;
+	}
 
-	    public GraduadoDTO findByIdToDTO(Long id) {
-	        Graduado estudiante = graduadoRepository.findById(id)
-	                .orElseThrow(() -> new ResourceNotFoundException("id", id));
+	@Override
+	public List findAll() {
+		return graduadoRepository.findAll().stream()
+				.peek(e -> e.setUrl_pdf(e.getRuta_pdf() == null ? null : s3Service.getObjectUrl(e.getRuta_pdf())))
+				.map(e -> mapToDTO(e)).collect(Collectors.toList());
+	}
 
-	        return mapToDTO(estudiante);
-	    }
+	public GraduadoDTO findByIdToDTO(Long id) {
+		Graduado estudiante = graduadoRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("id", id));
 
-	    public GraduadoDTO findByUsuario(long id_usuario) {
+		return mapToDTO(estudiante);
+	}
 
-	        Graduado estudiante = graduadoRepository.findByUsuario(id_usuario)
-	                .orElseThrow(() -> new ResourceNotFoundException("id_usuario", id_usuario));
-	        estudiante.setUrl_pdf(estudiante.getRuta_pdf() == null ? null : s3Service.getObjectUrl(estudiante.getRuta_pdf()));
-	        return mapToDTO(estudiante);
-	    }
+	public GraduadoDTO findByUsuario(long id_usuario) {
 
-	  
+		Graduado estudiante = graduadoRepository.findByUsuario(id_usuario)
+				.orElseThrow(() -> new ResourceNotFoundException("id_usuario", id_usuario));
+		estudiante
+				.setUrl_pdf(estudiante.getRuta_pdf() == null ? null : s3Service.getObjectUrl(estudiante.getRuta_pdf()));
+		return mapToDTO(estudiante);
+	}
+	
+	public Graduado findByUsuarioPersonaCedulaContaining(String cedula) {
+		return graduadoRepository.findByUsuarioPersonaCedulaContaining(cedula)
+				.orElseThrow(() -> new ResourceNotFoundException("cedula", cedula));
+	}
 
-	    @Override
-	    public Graduado save(Object entity) {
+	@Override
+	public Graduado save(Object entity) {
 
-	        return graduadoRepository.save(mapToEntity((GraduadoDTO) entity));
-	    }
+		return graduadoRepository.save(mapToEntity((GraduadoDTO) entity));
+	}
 
-	    public Long countEstudiantes(){
-	        return graduadoRepository.count();
-	    }
+	public Long countEstudiantes() {
+		return graduadoRepository.count();
+	}
 
-  }
+}
