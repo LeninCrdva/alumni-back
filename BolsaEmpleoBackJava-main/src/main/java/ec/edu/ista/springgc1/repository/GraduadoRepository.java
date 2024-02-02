e5af88a4f248d09c60d04a84e30f898c5d08103c
Evelyn Chichande <echichandeperea@gmail.com>
2024-02-02 01:04:22 -0500

    repository

--- BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/repository/GraduadoRepository.java
+++ BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/repository/GraduadoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ec.edu.ista.springgc1.model.entity.Graduado;
@@ -18,6 +19,9 @@ public interface GraduadoRepository extends GenericRepository<Graduado> {
 	Optional<Graduado> findByUsuarioPersonaCedulaContaining(String cedula);
 	
 	List<Graduado> findByEmailPersonalIn(Set<String> email_personal);
-	
+
 	Optional<Graduado> findByUsuarioNombreUsuario(String name);
+
+	@Query("SELECT g FROM Graduado g LEFT JOIN g.ofertas o WHERE o IS NULL")
+	List<Graduado> findAllGraduadosWithoutOfertas();
 }
