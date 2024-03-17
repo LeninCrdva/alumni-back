e73013ff905926288a7941dcd46059f4bdc45cb2
LeninCr <lenin.cordova.est@tecazuay.edu.ec>
2024-03-16 20:50:04 -0500

    Update Persona.java

--- BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/model/entity/Persona.java
+++ BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/model/entity/Persona.java
@@ -2,16 +2,12 @@ package ec.edu.ista.springgc1.model.entity;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @Entity
@@ -21,17 +17,32 @@ public class Persona {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "cod_perso")
 	private Long id;
+
 	@Column(name = "cedula", unique = true, nullable = false)
 	@Size(min = 10, max = 10, message = "La cédula debe tener exactamente 10 dígitos")
 	@Pattern(regexp = "\\d+", message = "La cédula debe contener solo dígitos")
 	private String cedula;
+
 	private String primer_nombre;
+
 	private String segundo_nombre;
+
 	@Column(name = "fechaNacimiento", nullable = false)
+	@DateTimeFormat(pattern = "YYYY-MM-dd")
 	private LocalDate fechaNacimiento;
+
 	@Size(min = 10, max = 10, message = "El número de celular debe tener exactamente 10 dígitos")
 	@Pattern(regexp = "\\d+", message = "El número de celular debe contener solo dígitos")
 	private String telefono;
+
 	private String apellido_paterno;
+
 	private String apellido_materno;
+
+	@Enumerated(EnumType.STRING)
+	private Sex sexo;
+
+	public static enum Sex {
+		MASCULINO, FEMENINO, OTRO;
+	}
 }
