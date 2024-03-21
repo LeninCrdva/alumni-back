c17bd8ec97599a410f89dccf8a1ba7356732e24e
LeninCr <lenin.cordova.est@tecazuay.edu.ec>
2024-03-20 20:49:28 -0500

    Engine template changed by Thymeleaf
    FreeMarker has switched to Thymeleaf for the email template.

--- BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/service/mail/EmailService.java
+++ BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/service/mail/EmailService.java
@@ -9,7 +9,6 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -23,142 +22,131 @@ import ec.edu.ista.springgc1.model.entity.Usuario;
 import ec.edu.ista.springgc1.service.impl.AdministradorServiceImpl;
 import ec.edu.ista.springgc1.service.impl.GraduadoServiceImpl;
 import ec.edu.ista.springgc1.service.impl.RecoveryTokenServiceImpl;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 public class EmailService {
-	@Autowired
-	private JavaMailSender sender;
 
-	@Autowired
-	private Configuration config;
+    @Autowired
+    private JavaMailSender sender;
 
-	@Autowired
-	private RecoveryTokenServiceImpl tokenService;
+    @Autowired
+    private ITemplateEngine templateEngine;
 
-	@Autowired
-	private GraduadoServiceImpl graduadoService;
+    @Autowired
+    private RecoveryTokenServiceImpl tokenService;
 
-	@Autowired
-	private AdministradorServiceImpl graduAdministradorServiceImpl;
+    @Autowired
+    private GraduadoServiceImpl graduadoService;
 
-	@Autowired
-	private RecoveryPasswordToken tokenPasswordRecovery;
+    @Autowired
+    private AdministradorServiceImpl graduAdministradorServiceImpl;
 
-	@Value("${angular.recovery.url}")
-	private String RECOVERY_URL;
+    @Autowired
+    private RecoveryPasswordToken tokenPasswordRecovery;
 
-	public MailResponse sendEmail(MailRequest request, Map<String, Object> model) {
-		MailResponse response = new MailResponse();
-		MimeMessage message = sender.createMimeMessage();
-		try {
-			MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-					StandardCharsets.UTF_8.name());
+    @Value("${angular.recovery.url}")
+    private String RECOVERY_URL;
 
-			String emailCase = request.getCaseEmail();
+    public MailResponse sendEmail(MailRequest request, Map<String, Object> model) {
+        MailResponse response = new MailResponse();
+        MimeMessage message = sender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
 
-			Template t = getTemplate(emailCase);
+            getHtml(request, model, response, message, helper);
+        } catch (MessagingException | IOException e) {
+            response.setMessage("Fallo al enviar email: " + e.getMessage());
+            response.setStatus(Boolean.FALSE);
+        }
 
-			if (t != null) {
-				getHtml(request, model, response, message, helper, t);
-			} else {
-				response.setMessage("No se pudo enviar el correo, no hay caso específico");
-				response.setStatus(Boolean.FALSE);
-			}
-		} catch (MessagingException | IOException | TemplateException e) {
-			response.setMessage("Fallo al enviar email: " + e.getMessage());
-			response.setStatus(Boolean.FALSE);
-		}
+        return response;
+    }
 
-		return response;
-	}
+    public MailResponse sendRecoveryEmail(MailRequest request, Map<String, Object> model) {
+        MailResponse response = new MailResponse();
+        MimeMessage message = sender.createMimeMessage();
+        RecoveryToken token = generateRecoveryToken(request.getTo());
+        String activationUrl = RECOVERY_URL + "?reset_token=" + token.getToken();
 
-	public MailResponse sendRecoveryEmail(MailRequest request, Map<String, Object> model) {
-		MailResponse response = new MailResponse();
-		MimeMessage message = sender.createMimeMessage();
-		RecoveryToken token = generateRecoveryToken(request.getTo());
-		String activationUrl = RECOVERY_URL + "?reset_token=" + token.getToken();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
 
-		try {
-			MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-					StandardCharsets.UTF_8.name());
+            model.put("URL", activationUrl);
 
-			String emailCase = request.getCaseEmail();
+            getHtml(request, model, response, message, helper);
 
-			Template t = getTemplate(emailCase);
+        } catch (MessagingException | IOException e) {
+            response.setMessage("Fallo al enviar email: " + e.getMessage());
+            response.setStatus(Boolean.FALSE);
+        }
 
-			model.put("URL", activationUrl);
-			getHtml(request, model, response, message, helper, t);
-		} catch (MessagingException | IOException | TemplateException e) {
-			response.setMessage("Fallo al enviar email: " + e.getMessage());
-			response.setStatus(Boolean.FALSE);
-		}
+        return response;
+    }
 
-		return response;
-	}
+    private void getHtml(MailRequest request, Map<String, Object> model, MailResponse response, MimeMessage message, MimeMessageHelper helper) throws IOException, MessagingException {
+        Context context = new Context();
+        context.setVariables(model);
 
-	private void getHtml(MailRequest request, Map<String, Object> model, MailResponse response, MimeMessage message, MimeMessageHelper helper, Template t) throws IOException, TemplateException, MessagingException {
-		String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+        String html = templateEngine.process(getTemplate(request.getCaseEmail()), context);
 
-		helper.setTo(request.getTo());
-		helper.setText(html, true);
-		helper.setSubject(request.getSubject());
-		helper.setFrom(request.getFrom());
-		sender.send(message);
+        helper.setTo(request.getTo());
+        helper.setText(html, true);
+        helper.setSubject(request.getSubject());
+        helper.setFrom(request.getFrom());
+        sender.send(message);
 
-		response.setMessage("Email enviado a: " + request.getTo());
-		response.setStatus(Boolean.TRUE);
-	}
+        response.setMessage("Email enviado a: " + request.getTo());
+        response.setStatus(Boolean.TRUE);
+    }
 
-	private Template getTemplate(String emailCase) {
-		Template t;
+    private static String getTemplate(String emailCase) {
+        String t;
 
-		try {
-			switch (emailCase) {
-				case "postulate":
-					t = config.getTemplate("graduate/email-template-postulate.ftl");
-					break;
-				case "remove-postulate":
-					t = config.getTemplate("graduate/email-template-remove-postulate.ftl");
-					break;
-				case "accept-postulate":
-					t = config.getTemplate("graduate/email-template-accept-postulate.ftl");
-					break;
-				case "reset-password":
-					t = config.getTemplate("graduate/email-template-recovery-email.ftl");
-					break;
-				case "list-postulates":
-					t = config.getTemplate("businessman/email-template-list-postulates.ftl");
-					break;
-				default:
-					t = config.getTemplate("graduate/email-template-contact-us.ftl");
-					break;
-			}
-		} catch (IOException e) {
-			t = null;
-		}
+        switch (emailCase) {
+            case "postulate":
+                t = "graduate/email-template-postulate";
+                break;
+            case "remove-postulate":
+                System.out.println("Email case: " + emailCase);
+                t = "graduate/email-template-remove-postulate";
+                break;
+            case "accept-postulate":
+                t = "graduate/email-template-accept-postulate";
+                break;
+            case "reset-password":
+                t = "graduate/email-template-recovery-email";
+                break;
+            case "list-postulates":
+                t = "businessman/email-template-list-postulates";
+                break;
+            default:
+                t = "graduate/email-template-contact-us";
+                break;
+        }
 
-		return t;
-	}
+        return t;
+    }
 
-	private RecoveryToken generateRecoveryToken(String email) {
-	    String tokenValue = tokenPasswordRecovery.generateToken();
-	    RecoveryToken recoveryToken = new RecoveryToken();
-	    Usuario usuario = graduadoService.findByEmail(email).getUsuario();
-	    Date expirationDate = tokenPasswordRecovery.calculateExpirationDate();
-	    
-	    if (usuario != null && !usuario.getNombreUsuario().isEmpty()) {
-	        recoveryToken.setUsuario(usuario);
-	    } else {
-	        usuario = graduAdministradorServiceImpl.findByEmail(email).getUsuario();
-	        recoveryToken.setUsuario(usuario);
-	    }
-	    
-	    recoveryToken.setToken(tokenValue);
-	    recoveryToken.setExpiration(expirationDate);
+    private RecoveryToken generateRecoveryToken(String email) {
+        String tokenValue = tokenPasswordRecovery.generateToken();
+        RecoveryToken recoveryToken = new RecoveryToken();
+        Usuario usuario = graduadoService.findByEmail(email).getUsuario();
+        Date expirationDate = tokenPasswordRecovery.calculateExpirationDate();
+
+        if (usuario != null && !usuario.getNombreUsuario().isEmpty()) {
+            recoveryToken.setUsuario(usuario);
+        } else {
+            usuario = graduAdministradorServiceImpl.findByEmail(email).getUsuario();
+            recoveryToken.setUsuario(usuario);
+        }
+
+        recoveryToken.setToken(tokenValue);
+        recoveryToken.setExpiration(expirationDate);
 
         return tokenService.save(recoveryToken);
-	}
+    }
 }
