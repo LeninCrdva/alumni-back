3a0a5f98773e76cec84ef066a9942192e7647df8
LeninCr <lenin.cordova.est@tecazuay.edu.ec>
2024-03-31 22:11:30 -0500

    Cambios en presentación de información

--- BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/model/dto/OfertasLaboralesDTO.java
+++ BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/model/dto/OfertasLaboralesDTO.java
@@ -2,38 +2,54 @@ package ec.edu.ista.springgc1.model.dto;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import ec.edu.ista.springgc1.view.View;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 public class OfertasLaboralesDTO implements Serializable {
 
+    @JsonView({View.Postulacion.class, View.Public.class})
     private Long id;
 
+    @JsonView({View.Postulacion.class, View.Public.class})
     private double salario;
 
     @DateTimeFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+    @JsonView({View.Postulacion.class, View.Public.class})
     private LocalDateTime fechaCierre;
 
     @DateTimeFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+    @JsonView({View.Postulacion.class, View.Public.class})
     private LocalDateTime fechaPublicacion;
 
+    @JsonView(View.Postulacion.class)
     private String cargo;
 
-    private String tiempo; 
+    @JsonView({View.Postulacion.class, View.Public.class})
+    private String tiempo;
 
+    @JsonView({View.Postulacion.class, View.Public.class})
     private String experiencia;
 
     @DateTimeFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+    @JsonView({View.Postulacion.class, View.Public.class})
     private LocalDateTime fechaApertura;
 
+    @JsonView({View.Postulacion.class, View.Public.class})
     private String areaConocimiento;
 
+    @JsonView({View.Postulacion.class, View.Public.class})
     private Boolean estado;
 
+    @JsonView({View.Postulacion.class, View.Public.class})
     private String nombreEmpresa;
-    
+
+    @JsonView({View.Postulacion.class, View.Public.class})
     private String fotoPortada;
 
+    @JsonView({View.Postulacion.class, View.Public.class})
     private String tipo;
 }
--- BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/model/entity/OfertasLaborales.java
+++ BolsaEmpleoBackJava-main/src/main/java/ec/edu/ista/springgc1/model/entity/OfertasLaborales.java
@@ -18,52 +18,57 @@ public class OfertasLaborales {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "oferta_id")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private Long id;
 
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private double salario;
 
 	@DateTimeFormat(pattern = "YYYY-MM-dd HH:mm:ss")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private LocalDateTime fechaCierre;
 
 	@DateTimeFormat(pattern = "YYYY-MM-dd HH:mm:ss")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private LocalDateTime fechaPublicacion;
 	
 	@ColumnTransformer(write = "UPPER(?)")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private String cargo;
 	
 	@ColumnTransformer(write = "UPPER(?)")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private String tiempo;
 
 	@ColumnTransformer(write = "UPPER(?)")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private String experiencia;
 
 	@DateTimeFormat(pattern = "YYYY-MM-dd HH:mm:ss")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private LocalDateTime fechaApertura;
 
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private String areaConocimiento;
 
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private Boolean estado;
 	
 	@ManyToOne
 	@JoinColumn(name = "id_empresa", referencedColumnName = "id_empresa")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private Empresa empresa;
 	
 	@Column(name = "tipo")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private String tipo;
 	
 	@Column(name = "foto_portada", columnDefinition = "TEXT")
-	@JsonView(View.Postulacion.class)
+	@JsonView({View.Postulacion.class, View.Public.class})
 	private String fotoPortada;
+
+	@PrePersist
+	public void prePersist() {
+		fechaPublicacion = LocalDateTime.now();
+	}
 }
